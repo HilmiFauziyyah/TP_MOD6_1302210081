63cac1b401397f0c945b483833b5115067f4405a
hilmifauziyyah <hilmifs2113@gmail.com>
2023-03-27 00:45:28 +0700

    menambahkan class SayaTubeVideo

--- TP_MOD6_1302210081/TP_MOD6_1302210081/Program.cs
+++ TP_MOD6_1302210081/TP_MOD6_1302210081/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TP_MOD6_1302210081
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            SayaTubeVideo saya = new SayaTubeVideo(" Tutorial Design By Contract - Hilmi Fauziyyah S ");
+
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ TP_MOD6_1302210081/TP_MOD6_1302210081/SayaTubeVideo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MOD6_1302210081
+{
+    class SayaTubeVideo
+    {
+        private int id;
+
+        private String title;
+
+        private int playCount;
+
+        public SayaTubeVideo(String title)
+        {
+            this.title = title;
+
+            var a = this.title.Substring(0, Math.Min(100, this.title.Length));
+
+            Contract.Requires(this.title != null);
+
+            String number = "";
+
+            Random rnd = new Random();
+
+            id = rnd.Next(1, 100000);
+
+            number += id.ToString("D5");
+
+            playCount = 0;
+
+        }
+
+        public void PrintVideoDetails()
+        {
+
+            Console.WriteLine("ID : " + id);
+
+            Console.WriteLine("Title : " + title);
+
+            Console.WriteLine("playCount : " + playCount);
+
+        }
+    }
